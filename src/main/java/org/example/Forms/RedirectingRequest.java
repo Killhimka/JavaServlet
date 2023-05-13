64b123a4ac3637f229920730072620581fdb1a5c
killhimka <bum_911@mail.ru>
2023-05-13 14:59:13 +0300

    13.05.2023

---
+++ src/main/java/org/example/Forms/RedirectingRequest.java
@@ -0,0 +1,21 @@
+package org.example.Forms;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+// Перенаправление на другой адрес
+@WebServlet("/request")
+public class RedirectingRequest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = "/index.html";
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+        requestDispatcher.forward(req,resp);
+    }
+}
