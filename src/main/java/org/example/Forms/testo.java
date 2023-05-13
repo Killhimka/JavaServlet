2fdbdd08b501974581dd49353f3bb657af40e702
killhimka <bum_911@mail.ru>
2023-05-13 15:05:14 +0300

    13.05.2023

---
+++ src/main/java/org/example/Forms/testo.java
@@ -0,0 +1,14 @@
+package org.example.Forms;
+
+public class testo {
+    public static void main(String[] args) {
+
+        String i = "/";
+        int a = 6;
+        int b = 2;
+        if (i == "/")
+            System.out.println(a/b);
+        else if (i=="*")
+            System.out.println(a*b);
+    }
+}
