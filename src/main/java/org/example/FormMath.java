e376801d98dad3149eb6f3a59bc976d0e847cf6b
killhimka <bum_911@mail.ru>
2023-05-14 19:08:19 +0300

    14.05.2023

--- src/main/java/org/example/FormMath.java
+++ src/main/java/org/example/FormMath.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Type;
 
 @WebServlet("/math")
 public class FormMath extends HttpServlet {
@@ -16,14 +17,21 @@ public class FormMath extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
 
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/hello");
+
         int number1 = Integer.valueOf(req.getParameter("number1"));
         Integer number2 = Integer.valueOf(req.getParameter("number2"));
         String action = req.getParameter("action");
         try {
             if (action.contains("*"))
             printWriter.println(number1*number2);
-            else if (action.contains("/"))
-            printWriter.println(number1/number2);
+            else if (action.contains("/")) {
+                if (number2==0){
+                    requestDispatcher.forward(req,resp);
+                }
+                printWriter.println(number1 / number2);
+            }
             else if (action.contains("+"))
             printWriter.println(number1+number2);
             else printWriter.println(number1-number2);
