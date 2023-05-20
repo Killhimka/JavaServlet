a3063c4a11ada8f84ed71c358cb5497627495fdf
killhimka <bum_911@mail.ru>
2023-05-20 17:45:49 +0300

    17.05.2023

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 2041275512219239990l;
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+        this.name = "";
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
